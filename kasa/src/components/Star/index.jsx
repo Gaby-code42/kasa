a70e4c9d499f78886cdb0ceadbf9a90b5fa4df22
RaphaelB <raphael.bonacina@hotmail.fr>
2024-09-24 15:39:15 +0200

    fonctionnalité ok de la page housing

---
+++ kasa/src/components/Star/index.jsx
@@ -0,0 +1,26 @@
+import inactiveStar from '../../Assets/star_inactive.png';
+import activeStar from '../../Assets/star.png';
+import Data from '../../Data/index.json'
+import { useParams } from 'react-router-dom';
+
+function StarRating (){
+    const { title } = useParams();
+    const decodedTitle = decodeURIComponent(title);
+    const CardData = Data.find((Card) => Card.title === decodedTitle);
+    const rating = CardData.rating;
+
+    return (
+        <div>
+
+    {[...Array(5)].map((_, index) => (
+        <img
+            key={index}
+            src={index < rating ? activeStar : inactiveStar} 
+            alt={`star-${index}`}
+        />
+    ))}
+    </div>
+)
+}
+
+export default StarRating
--- kasa/src/pages/HousingDetails/index.jsx
+++ kasa/src/pages/HousingDetails/index.jsx
@@ -3,32 +3,52 @@ import { useParams } from 'react-router-dom';
 import Accordion from "../../components/Accordion"
 import Data from '../../Data/index.json'
 import Carousel from "../../components/Slider";
+import StarRating from "../../components/Star";
 import './style.scss'
+
 
 
 function HousingDetails (){
     const { title } = useParams();
     const decodedTitle = decodeURIComponent(title);
     const CardData = Data.find((Card) => Card.title === decodedTitle);
-    const rating = CardData ? CardData.rating : [];
+    const hostName = CardData.host?.name;
+    const hostPicture = CardData.host?.picture;
+    
 
     return(
-
     <div>
-        <div>
-            <Carousel images = {CardData.pictures} />        
-        </div>
+        
+        <Carousel images = {CardData.pictures} />        
 
-        <Accordion title="Description" content={CardData.description}/>
-    
-        <Accordion title="Équipements" content={
-            <ul>
-                {CardData.equipments.map((equipment, index) => (
-                    <li key={index}>{equipment}</li>
-                ))}
-            </ul>
-            }
-        />
+
+        <div className="Housing__Title">
+            <h2>{title}</h2>
+            <p>{CardData.location}</p>
+        </div>
+        <div className="Housing__SellerProfil">
+            <h3>{hostName}</h3>
+            <img src={hostPicture} alt={hostPicture}/>
+        </div>
+        <div className="Housing__Tag">
+            {CardData.tags.map((tag, index) => (
+                <span key={index} classname='tags'>
+                    {tag}
+                </span>
+            ))}
+        </div>
+            <StarRating className="Housing__Rating"/>
+        <div>
+            <Accordion title="Description" content={CardData.description}/>
+            <Accordion title="Équipements" content={
+                <ul>
+                    {CardData.equipments.map((equipment, index) => (
+                        <li key={index}>{equipment}</li>
+                    ))}
+                </ul>
+                }
+            />
+        </div>
     </div>
 
 
